11df134d47f47accd76c86d66b1a2b5c21e8d275
rikodwi <riko.aa.aa.aa.aa@gmail.com>
2022-09-08 10:19:07 +0700

    j

--- Glade/modules/help.py
+++ Glade/modules/help.py
@@ -33,7 +33,7 @@ async def module_help(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n•[GladeSupport](t.me/Gladesupportgroup)•"
+            message, f"```{str(ac)}```\n•[support](t.me/Gladesupportgroup) x [group](t.me/Gladesupporchannel)•"
         )
         await message.reply(
             f"**Contoh Ketik** `{CMD_HANDLER}help afk` **Untuk Melihat Informasi Module**"
